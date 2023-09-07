38c0e061a04a19ba2e8e6d8580c79118fd01a367
Replit user <>
2023-09-07 22:51:56 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -81,12 +81,11 @@ void enlarge(HashMap * map) {
 
 void eraseMap(HashMap * map,  char * key) {    
   Pair* pair = searchMap(map,key);
-if pair != NULL;
+if pair != NULL;{
+  
   Pair-> key == NULL;
-  map -> size ;
-
-
-
+  map -> size --;
+}
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
