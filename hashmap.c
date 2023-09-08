2fc8745c347496688f24e7e3f7d31ea1596f2e2f
Replit user <>
2023-09-08 00:39:50 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -64,7 +64,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
-  pair**old_buckets = map-> buckets;
+  Pair**old_buckets = map-> buckets;
   map->capacity *= 2;
   map->buckets = (Pair **)calloc(map->capacity, sizeof(Pair *));
   map->size = 0; 
